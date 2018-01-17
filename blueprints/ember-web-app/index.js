4633709ef5c94eab58e7f3924c1b357f73112724
lypborges <lypborges@gmail.com>
2018-01-16 22:12:07 -0200

    Remove after install

--- blueprints/ember-web-app/index.js
+++ blueprints/ember-web-app/index.js
@@ -10,7 +10,7 @@ module.exports = {
     };
   },
 
-  afterInstall: function() {
-    return this.removePackageFromProject('ember-web-app-rename');
-  }
+  // afterInstall: function() {
+  //   return this.removePackageFromProject('ember-web-app-rename');
+  // }
 };
